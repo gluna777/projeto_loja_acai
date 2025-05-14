ddf77ffc07efc0a0e1cd69af98f70ceb32cfef64
Gustavo Luna Prcopio <gugalunaprocopio@gmail.com>
2025-05-14 01:02:04 -0300

    Adicionando cabeçalho ao arquivo index

--- index.php
+++ index.php
@@ -1,22 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Peça seu Açaí Já</title>
-    <!-- Bootstrap -->
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" 
-     rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" 
-     crossorigin="anonymous">
-    <!-- Font Awesome -->
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" 
-     integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" 
-     crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- App CSS -->
-    <link rel="stylesheet" href="css/styles.css">
-</head>
-<body>
+<?php
+    include_once("templates/header.php");
+?>
     <h1>Testando CSS</h1>
+    <i class="fas fa-sync-alt"></i>
     <!-- Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js" 
     integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" 
