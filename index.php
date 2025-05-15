5433e95b047c7f9b5e8aed1cf5106a68f3773c80
Gustavo Luna Prcopio <gugalunaprocopio@gmail.com>
2025-05-15 15:01:42 -0300

    Configurando o arquivo index

--- index.php
+++ index.php
@@ -1,8 +1,47 @@
 <?php
     include_once("templates/header.php");
 ?>
-    <h1>Testando CSS</h1>
-    <i class="fas fa-sync-alt"></i>
+    <div id="main-banner">
+        <h1>Faça seu Pedido</h1>
+    </div>
+    <div id="main-container">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                <h2>Monte seu açaí como desejar:</h2>
+                <form action="process/acai.php" method="POST" id="acai-form">
+                    <div class="form-group">
+                        <label for="sabor">Sabor:</label>
+                        <select name="sabor" id="sabor" class="form-control">
+                            <option value="">Selecione o sabor</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="creme">Creme:</label>
+                        <select name="creme" id="creme" class="form-control">
+                            <option value="">Selecione o creme</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="frutas">Frutas: (Máximo 3)</label>
+                        <select multiple name="frutas[]" id="frutas" class="form-control">
+                            <option value="">Selecione o frutas</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="complementos">Complementos: (Máximo 3)</label>
+                        <select multiple name="complementos[]" id="complementos" class="form-control">
+                            <option value="">Selecione o complementos</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" class="btn btn-primary" value="Fazer Pedido!">
+                    </div>
+                </form>
+                </div>
+            </div>
+        </div>
+    </div>
 <?php
     include_once("templates/footer.php");
 ?>
