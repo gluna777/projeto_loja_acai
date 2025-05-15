819990b03a3165175ea605be4caa3ceb4792e7c2
Gustavo Luna Prcopio <gugalunaprocopio@gmail.com>
2025-05-15 17:57:15 -0300

    Adicionando itens do banco de dados local a home

--- index.php
+++ index.php
@@ -12,27 +12,46 @@
                 <h2>Monte seu açaí como desejar:</h2>
                 <form action="process/acai.php" method="POST" id="acai-form">
                     <div class="form-group">
+                        <label for="tamanho">Tamanho:</label>
+                        <select name="tamanho" id="tamanho" class="form-control">
+                            <option value="">Selecione o tamanho</option>
+                            <?php foreach ($tamanhos as $tamanho): ?>
+                                <option value="<?= $tamanho["id_tamanho"] ?>"> <?= $tamanho["copo"] ?> </option>
+                            <?php endforeach ?>
+                        </select>
                         <label for="sabor">Sabor:</label>
                         <select name="sabor" id="sabor" class="form-control">
                             <option value="">Selecione o sabor</option>
+                            <?php foreach ($sabores as $sabor): ?>
+                                <option value="<?= $sabor["id_sabor"] ?>"> <?= $sabor["sabor"] ?> </option>
+                            <?php endforeach ?>
                         </select>
                     </div>
                     <div class="form-group">
                         <label for="creme">Creme:</label>
                         <select name="creme" id="creme" class="form-control">
                             <option value="">Selecione o creme</option>
+                            <?php foreach ($cremes as $creme): ?>
+                                <option value="<?= $creme["id_creme"] ?>"> <?= $creme["nome"] ?> </option>
+                            <?php endforeach ?>
                         </select>
                     </div>
                     <div class="form-group">
-                        <label for="frutas">Frutas: (Máximo 3)</label>
+                        <label for="frutas">Frutas (Máximo 3):</label>
                         <select multiple name="frutas[]" id="frutas" class="form-control">
                             <option value="">Selecione o frutas</option>
+                            <?php foreach ($frutas as $fruta): ?>
+                                <option value="<?= $fruta["id_fruta"] ?>"> <?= $fruta["nome"] ?> </option>
+                            <?php endforeach ?>
                         </select>
                     </div>
                     <div class="form-group">
-                        <label for="complementos">Complementos: (Máximo 3)</label>
+                        <label for="complementos">Complementos (Máximo 3):</label>
                         <select multiple name="complementos[]" id="complementos" class="form-control">
                             <option value="">Selecione o complementos</option>
+                            <?php foreach ($complementos as $complemento): ?>
+                                <option value="<?= $complemento["id_complemento"] ?>"> <?= $complemento["nome"] ?> </option>
+                            <?php endforeach ?>
                         </select>
                     </div>
                     <div class="form-group">
