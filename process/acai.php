4ba2e46c7e4936048beeee422d414f38f98490ec
Gustavo Luna Prcopio <gugalunaprocopio@gmail.com>
2025-05-15 17:56:08 -0300

    Importando tabelas do Banco de dados local

--- process/acai.php
+++ process/acai.php
@@ -2,4 +2,29 @@
 
     include_once("connection.php");
 
+    $method = $_SERVER["REQUEST_METHOD"];
+
+    //Resgate dos dados, montagem do pedido
+    if ($method === "GET") {
+
+        $tamanhosQuery = $conn->query("SELECT * FROM tamanhos;");
+        $tamanhos  = $tamanhosQuery->fetchAll();
+
+        $saboresQuery = $conn->query("SELECT * FROM sabores;");
+        $sabores  = $saboresQuery->fetchAll();
+
+        $cremesQuery = $conn->query("SELECT * FROM cremes;");
+        $cremes  = $cremesQuery->fetchAll();
+
+        $frutasQuery = $conn->query("SELECT * FROM frutas;");
+        $frutas  = $frutasQuery->fetchAll();
+
+        $complementosQuery = $conn->query("SELECT * FROM complementos;");
+        $complementos  = $complementosQuery->fetchAll();
+
+    } 
+    else if ($method === "POST") {
+
+    }
+
 ?>
